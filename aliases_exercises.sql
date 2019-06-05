89fec467d35b6cf931dc370ce8501fef39cecaf8
Nic Tipton <ngtipton@gmail.com>
2019-06-05 16:19:53 -0500

    Adding exercise to git

---
+++ aliases_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+# SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date as DOB
+# FROM employees
+# LIMIT 10;
+
+SHOW TABLES;
+
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE salaries;
+DESCRIBE titles;
